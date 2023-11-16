eaf93a19cd974f3ed463ac0248c4b961effa253a
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-16 20:56:40 +0100

    initial commit

---
+++ UD27EJ1/src/main/java/com/example/demo/Ud27Ej1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud27Ej1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud27Ej1Application.class, args);
+	}
+
+}
---
+++ UD27EJ1/src/test/java/com/example/demo/Ud27Ej1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud27Ej1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
